9e580ae44d1833eb01907bb1e446d4d496f6c238
Mario Hermiz <134554555+mhermiz@users.noreply.github.com>
2025-09-16 20:18:36 -0700

    Added more things
    - Added Collectible script feature
    - Added glow effect to sprites
    - Fixed a couple more things

---
+++ Assets/Scripts/Interactable/Collectible.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private Animator anim;
+    private bool collected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+        if (!collected && other.CompareTag("Player"))
+        {
+            collected = true;
+            Debug.Log("collected");
+            
+            // Play animation
+            if (anim != null)
+            {
+                anim.SetTrigger("Collect");
+            }
+
+            // Destroy after animation plays
+            float delay = anim != null ? anim.GetCurrentAnimatorStateInfo(0).length : 0.2f;
+            Destroy(gameObject, delay);
+        }
+	}
+}
