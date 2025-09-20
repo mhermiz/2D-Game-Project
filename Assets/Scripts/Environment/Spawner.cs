970e0f635d40eb730ca7b16b5c5427cad1d2eb9e
Mario Hermiz <134554555+mhermiz@users.noreply.github.com>
2025-09-19 17:44:10 -0700

    Fixed Some Stuff

--- Assets/Scripts/Environment/Spawner.cs
+++ Assets/Scripts/Environment/Spawner.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
